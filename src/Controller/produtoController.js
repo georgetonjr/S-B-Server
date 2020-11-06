d6c3fb0d787c6a7f66f4d87cf03d29a238bb0a5a
georgetonjr <37754623+georgetonjr@users.noreply.github.com>
2020-11-06 20:07:33 -0300

    testing product creator

--- src/Controller/produtoController.js
+++ src/Controller/produtoController.js
@@ -15,33 +15,28 @@ module.exports = {
     let buffer = new Buffer(matches[2], 'base64');
 
     //save image to azure storage
-    await blobSvc.createBlockBlobFromText('productsbb', filename, buffer, {
-      contentSettings:{contentType: type}
+    blobSvc.createBlockBlobFromText('productsbb', filename, buffer, {
+      contentSettings: { contentType: type }
     }, (error, result, response) => {
-      if(error){
-        console.log(error)
-        filename = 'default.png'
+      if (error) {
+        console.log(error);
+        filename = 'default.png';
       }
     });
     console.log('https://productsb.blob.core.windows.net/productsbb/' + filename)
 
     console.log(req.body)
-    /*try{
-      const produto = await Produto.create({
-        img: 'https://productsb.blob.core.windows.net/productsbb/' + filename, 
-        codigo, 
-        valor, 
-        fabricante, 
-        quantestoque, 
-        parceiro
-      })
-      
-      return res.send('chegou');
-    }
-    catch{
-      return res.status(400).send({error: 'Registration failed!!'})
-    }
-    */
+    
+    Produto.create({
+      img: 'https://productsb.blob.core.windows.net/productsbb/' + filename, 
+      codigo, 
+      valor, 
+      fabricante, 
+      quantestoque, 
+      parceiro
+    })
+    .then(() => res.status(200).json('Product successfully registered'))
+    .catch(() => res.status(400).json({error: 'Registration failed!!'}));
   },
 
   async show(req, res){
