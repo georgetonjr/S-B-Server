7e97ed1411ff35656e7111bff3319bf18d796fc5
georgetonjr <37754623+georgetonjr@users.noreply.github.com>
2020-11-16 15:27:53 -0300

    Update VoucherController.js

--- src/Controller/VoucherController.js
+++ src/Controller/VoucherController.js
@@ -3,9 +3,12 @@ const Voucher = require('./../Models/Voucher');
 module.exports = {
   async store(req, res){
     console.log(req.body)
-    const {} = req.body;
+    const { parceiro, cliente, produto } = req.body;
     Voucher.create({
-
+      parceiro,
+      produto,
+      cliente,
+      voucherNumber: Math.random(),
     })
     .then(e => console.log(e))
     .catch(err => console.error(err))
